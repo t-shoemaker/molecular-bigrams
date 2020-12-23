554b3832ed4565fb1ff52347e2393f44d15848db
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2020-12-23 13:11:24 -0500

    quote tweaks

--- markov-bigrams/markovBigrams.R
+++ markov-bigrams/markovBigrams.R
@@ -1,11 +1,11 @@
-packages <- c('igraph', 'ggplot2', 'ggraph')
+packages <- c("igraph", "ggplot2", "ggraph")
 lapply(packages, library, character.only=TRUE, warn.conflicts=FALSE)
 
-df <- read.csv('../data/google-books-transition-frequencies.csv', header=TRUE, row.names=1)
+df <- read.csv("../data/google-books-transition-frequencies.csv", header=TRUE, row.names=1)
 
 make_bigrams <- function(word) {
   bigram_vec = c()
-  chars <- strsplit(tolower(word), split='')[[1]]
+  chars <- strsplit(tolower(word), split="")[[1]]
   for (idx in seq_along(chars)) {
     while (idx < length(chars)) {
       bigram <- paste0(chars[idx], chars[idx+1])
@@ -19,7 +19,7 @@ make_bigrams <- function(word) {
 get_frequencies <- function(bigram_vec, freq_data) {
   bigram_freq = c()
   for (idx in seq_along(bigram_vec)) {
-    bigram <- strsplit(bigram_vec[idx], split='')[[1]]
+    bigram <- strsplit(bigram_vec[idx], split="")[[1]]
     pair <- freq_data[bigram[2], bigram[1]]
     bigram_freq <- c(bigram_freq, pair)
   }
@@ -36,17 +36,17 @@ make_network <- function(word, weight) {
 
 plot_network <- function(word, network) {
   set.seed(5)
-  chars <- strsplit(word, split='')[[1]]
-  ggraph(network, layout='linear') +
-    theme_graph(background='white') +
+  chars <- strsplit(word, split="")[[1]]
+  ggraph(network, layout="linear") +
+    theme_graph(background="white") +
     geom_edge_link(aes(edge_width=(I(frequencies)*100)),
                    show.legend=FALSE) +
     geom_node_point(size=0,
                     alpha=0,
                     x=-1) +
     geom_node_label(aes(label=chars),
-                    family='Verdana',
-                    color='black',
+                    family="Verdana",
+                    color="black",
                     size=8,
                     alpha=0,
                     y=-0.03,
